38d6b162d78e3edddad5c70145d25d93ee9e8ed9
AndyHernandez-Ksquare <andy.hernandez@theksquaregroup.com>
2024-07-01 09:51:55 -0400

    hotfix: Prisma filter switch case

--- src/prisma.filter.ts
+++ src/prisma.filter.ts
@@ -13,16 +13,19 @@ export class PrismaClientExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     let status = HttpStatus.BAD_REQUEST;
-
     let message = 'An error occurred';
-    if (exception.code === 'P2002') {
-      message = `Unique constraint violation on field(s): ${exception.meta.target}`;
-    } else if (exception.code === 'P2003') {
-      message = `Cannot create, delete or update: a foreign key constraint fails: ${exception.meta.field_name}`;
-    } else if (exception.code === 'P2025') {
-      status = HttpStatus.NOT_FOUND;
-      message = `${exception.meta.cause} ${exception.meta.modelName}`;
-      console.log(exception.meta);
+
+    switch (exception.code) {
+      case 'P2002':
+        message = `Unique constraint violation on field(s): ${exception.meta.target}`;
+        break;
+      case 'P2003':
+        message = `Cannot create, delete or update: a foreign key constraint fails: ${exception.meta.field_name}`;
+        break;
+      case 'P2025':
+        status = HttpStatus.NOT_FOUND;
+        message = `${exception.meta.cause} ${exception.meta.modelName}`;
+        break;
     }
 
     response.status(status).json({
